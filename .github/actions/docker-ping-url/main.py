2770710f4774411aa90727a1260d7a928fd6b52c
Ivan Fuentes <jivfur@gmail.com>
2026-01-12 14:21:54 -0500

    Adding what i didnt add to docker actions

--- .github/actions/docker-ping-url/main.py
+++ .github/actions/docker-ping-url/main.py
@@ -1,22 +1,30 @@
 import os
 import requests
-from time import sleep
+import time
 
-def ping_url(website_url, delay, max_trials):
+
+def set_output(file_path, key, value):
+    with open(file_path, 'a') as file:
+        print(f'{key}={value}', file=file)
+
+
+def ping_url(url, delay, max_trials):
     trials = 0
-    while trials< max_trials:
+
+    while trials < max_trials:
         try:
-            resp = requests.get(website_url)
-            if resp.status_code==200:
-                print(f"Website {website_url} is reachable")
+            response = requests.get(url)
+            if response.status_code == 200:
+                print(f"Website {url} is reachable.")
                 return True
         except requests.ConnectionError:
-            print(f"Website {website_url} is unreachable. Retrying in {delay} seconds")
-            sleep(delay)
-            trials+=1
+            print(f"Website {url} is unreachable. Retrying in {delay} seconds...")
+            time.sleep(delay)
+            trials += 1
         except requests.exceptions.MissingSchema:
-            print(f"Invalid URL format {website_url}")
+            print(f"Invalid URL format: {url}. Make sure the URL has a valid schema (e.g., http:// or https://)")
             return False
+    
     return False
 
 
@@ -24,10 +32,16 @@ def run():
     website_url = os.getenv("INPUT_URL")
     delay = int(os.getenv("INPUT_DELAY"))
     max_trials = int(os.getenv("INPUT_MAX_TRIALS"))
-    website_reachable = ping_url(website_url,delay, max_trials)
-    if not website_reachable:
-        raise Exception(f"Website {website_url} is malformed or unreachable")
-    print("Website {website_url} is reachable")
 
-if __name__=="__main__":
+    website_reachable = ping_url(website_url, delay, max_trials)
+
+    set_output(os.getenv('GITHUB_OUTPUT'), 'url-reachable', website_reachable)
+    
+    if not website_reachable:
+        raise Exception(f"Website {website_url} is malformed or unreachable.")
+
+    print(f"Website {website_url} is reachable.")
+
+
+if __name__ == "__main__":
     run()
